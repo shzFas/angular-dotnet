e1b08f716d694fd7f560914373b0ab89d0017912
shzfas <yuraka200486@gmail.com>
2023-01-15 16:19:04 +0600

    feat: swagger worked

---
+++ Api-angular/Controllers/TasksController.cs
@@ -0,0 +1,24 @@
+﻿using Api_angular.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_angular.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : Controller
+    {
+        private readonly FullStackDbContext _fullStackDbContext;
+
+        public TasksController(FullStackDbContext fullStackDbContext) {
+            _fullStackDbContext = fullStackDbContext;
+        }
+        [HttpGet] 
+        public async Task<IActionResult> GetAllTasks()
+        {
+            var tasks = await _fullStackDbContext.Tasks.ToListAsync();
+
+            return Ok(tasks);
+        }
+    }
+}
