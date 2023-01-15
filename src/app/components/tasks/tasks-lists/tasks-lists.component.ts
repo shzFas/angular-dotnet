0aaf58c11971eacdd1312ed090c346cfc3b0473d
shzfas <yuraka200486@gmail.com>
2023-01-15 17:04:24 +0600

    feat: list array in clg

--- src/app/components/tasks/tasks-lists/tasks-lists.component.ts
+++ src/app/components/tasks/tasks-lists/tasks-lists.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Tasks } from 'src/app/models/tasks.model';
+import { TasksService } from 'src/app/services/tasks.service';
 
 @Component({
   selector: 'app-tasks-lists',
@@ -8,29 +9,19 @@ import { Tasks } from 'src/app/models/tasks.model';
 })
 export class TasksListsComponent implements OnInit {
 
-  tasks: Tasks[] = [
-    {
-      id: '1',
-      name: 'Задача 1',
-      description: 'Отнеси подай принес иотнеси',
-      status: true,
-    },
-    {
-      id: '2',
-      name: 'Задача 2',
-      description: 'Отнесиподай принес иотнеси',
-      status: false,
-    },
-    {
-      id: '3',
-      name: 'Задача 3',
-      description: 'Отнесиподай принес иотнеси',
-      status: true,
-    },
-  ];
-  constructor() { }
+  tasks: Tasks[] = [];
+  constructor(private tasksService: TasksService) { }
 
   ngOnInit(): void {
+    this.tasksService.getAllTasks()
+      .subscribe({
+        next: (tasks) => {
+          console.log(tasks);
+        },
+        error: (response) => {
+          console.log(response);
+        }
+      });
   }
 
 }
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Tasks } from '../models/tasks.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
-  constructor() { }
+  baseApiUrl: string = environment.baseApiUrl
+  constructor(private http: HttpClient) { }
+
+  getAllTasks (): Observable<Tasks[]> {
+    return this.http.get<Tasks[]>(this.baseApiUrl + '/api/Tasks');
+  }
 }
